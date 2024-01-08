af3f737c679b41d17567cdd6c476ae5dabdf2892
hyebine <berryme015@gmail.com>
2024-01-08 17:29:56 +0900

    보일러플레이팅완공, basic 테마 사용, 불필요한 소스 제거, 플러그인, 폰트 설치, 최근게시글 메인에 올리기

---
+++ CHB/theme/basic/index.php
@@ -0,0 +1,18 @@
+<?php
+if (!defined('_INDEX_')) define('_INDEX_', true);
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+include_once(G5_THEME_PATH.'/head.php');
+?>
+
+
+
+<?php 
+
+	echo latest('mainswiper','adm_mainbanner',1, 100); 
+	?>
+
+
+<?php
+
+    // include_once(G5_THEME_PATH.'/tail.php');
